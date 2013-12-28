24ea4b8efb0e86a15b30436da098be29ef73c15c
xababafr <xavier.quelard@gmail.com>
2013-12-28 12:07:15 +0100

    DM - commit n°3
    - j'ai a peu pres tout changé
    - la structure de la matrice est bien plus simpliste et bien plus
    utile, meme si elle perds legerement en lisibilité (et encore)
    - j'ai ecrit la fonction effondrement, qui fait quasiment tout (il
    serait sans doute utiliser d'utiliser une ou deux fonctions
    auxilliaires)
    - il reste maintenant à la verifier,et plus de la réécrire?

--- DM 1/avalanche_3.py
+++ DM 1/avalanche_3.py
@@ -1,72 +1,107 @@
-Montagne =[ " |...... ",
-			" |*..... ",
-			" |*..... ",
-			" |*..... ",
-			" |**.... ",
-			" |***... ",
-			" |****.. ",
-			" |*****. ",
-			" |****** " ]
+from math import *
+from random import *
 
-# la matrice de la Montagne
-# les | representent le mur
-# les * representent les roches
-# les . representent du vide
+# que je suis bete ! cette structure correspondrait 
+#bien mieux ! il faut tout reprendre...
+
+Montagne = [ 8 , 5 , 4 , 3 , 2 , 1 , '!' ]
+
+# une seule obligation a ce format : un '!' a la fin
+# cependant, l'avantage est de pouvoir en mettre ailleurs
+# on suppose aussi que la montagne fait au moins une colonne d'epaiseur
 
 # compteur principal de carres ejectes
 
 compteur = 0
+length = len(Montagne)
 
-def element(ligne,colonne):
 
-	""" la fonction qui renvoi le contenu de la matrice à
-	    la ligne et la colonne souhaitee. On suppose que
-	    le mur est la colonne numero 1 et que la premiere
-	    ligne de la matrice est la premiere ligne egalement.
-	    La fonction renvoi 0 si c'est un mur, 1 si c'est un
-	    rocher, 2 si c'est un vide, et enfin 3 si ce n'est
-	    aucun des elements precedents (dans ce cas, cela
-	   	veut dire que l'on est dans la derniere colonne,
-	   	celle ou seront comptabilises les carres ejectes."""
+def colonne_instable():
 
-		# de par la structure choisie precedemment, selectionner
-		# un element precis est tres simple
+	""" retourne le numero de la premiere colonne de rochers
+		instables reperee, en partant de la gauche.
+		S'il n'y a pas de colonnes instables, renvoi False. """
 
-		element = Montagne[ligne-1,colonne]
+	precedente = Montagne[0]
 
-		# on retourne en fonction du contenu
+	# si la difference de hauteur est de deux, la colonne est instable
+	for i in range(1,length-1):
+		# 1er cas : si la colonne actuelle est plus grande
+		# (de 2 carres ou + ) que la precedente
+		if Montagne[i]-precedente > 2 :
+			return i # dans ce cas elle est instable
+		# 2eme cas : si la colonne actuelle est plus petite
+		# (de 2 carres ou + ) que la precedente
+		elif Montagne[i]-precedente < -2 : 
+			return i-1 # dans ce cas celle d'avant est instable
+	return False # sinon on retourne False
 
-		if element == "|" :
-			return 0
-		elif element == "*" :
-			return 1
-		elif element == "." :
-			return 2
-		elif element == " " :
-			return 3
-		else # erreur
-			return False
+def effondrement(rang):
 
-def modifier_element(ligne,colonne,contenu):
+	""" effectue l'effondrement de la colonne dont le numero
+		est specifie en parametre. Si le cube atteint un '!',
+		il est ejecte et comptabilise tel quel. """
 
-	""" remplace l'element de la matrice specifie par la
-		nouvelle valeur passee en argument. """
+	# si l'on est pas a l'une des deux etremites
+	if rang not in [0, length-1] : 
+		gauche = Montagne[rang-1]
+		droite = Montagne[rang+1]
+		# si il y a une denivelation + importante d'un coté comme de l'autre
+		if gauche != droite :
+			if gauche < droite :
+				#Montagne[rang-1] += 2
+				choix = rang-1
+			else :
+				#Montagne[rang+1] += 2
+				choix = rang+1
+		# ou si la dénivelation est la meme de chaque cote
+		else :
+			nbr = choice([-1,1])
+			#Montagne[rang+nbr] += 2
+			choix = rang+nbr
+	# sinon
+	else :
+		# si on est au debut
+		if rang == 0 :
+			choix = rang+1
+		# si on est a la fin, c'est des carres ejectes
+		else :
+			compteur += 2
 
-	if len(contenu) == 1:
-		Montagne[ligne-1,colonne] = "contenu" 
-	else
-		return False
+
+	# dans tous les cas de figure, la colonne actuelle perds 2 unites
+	Montagne[rang] -= 2
+
+	# puis, si la colonne suivante n'est pas un "conteneur" a cubes
+	if Montagne[choix] != '!' :
+		montagne[choix] += 2
+	# sinon, on les ejecte et on les comptabilisent
+	else :
+		compteur += 2
+
+
+
 
 # boucle principale
+# tant qu'il y a une colonne instable, on continue
+
+while colonne_instable() :
+
+	# 1/ chercher la premiere colonne instable de gauche a droite
+	colonne = colonne_instable()
+
+	# 2/ la faire s'effondrer
+	effondrement(colonne)
+
+
 
 # pour le moment, je ne sais pas trop quelle condition mettre,
 # alors on va partir sur une centaine d'iterations pour avoir
 # un systeme stable, ce qui sera largement suffisant
 
-for i in range(100):
+#for i in range(100):
 
 	# je pense qu'il faut proceder comme ceci : 
-	# 1/ chercher la premiere colonne instable de gauche a droite
 	# 2/ la faire s'effondrer
 	# 3/ puis recommencer
 	# 4/ avec a chaque effondrement une verification que le carre n'est
@@ -82,10 +117,7 @@ for i in range(100):
 
 	# enfin, il faut un compteur global pour les carres comptes
 	# je le met a l'exterieur de la boucle, juste apres la matrice,
-	# pour que chaque fonction y ait accès
-
-
-
+	# pour que chaque fonction y ait acces
 
 
 
