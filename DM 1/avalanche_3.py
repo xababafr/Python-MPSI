55d2afcb9c4c5dbd7b08d5cbad287dcd387c199a
xababafr <xavier.quelard@gmail.com>
2013-12-29 12:58:56 +0100

    Dm - commit n°11
    - Version 1.0

--- DM 1/avalanche_3.py
+++ DM 1/avalanche_3.py
@@ -2,9 +2,6 @@ from math import *
 from random import *
 import time
 
-# que je suis bete ! cette structure correspondrait 
-#bien mieux ! il faut tout reprendre...
-
 Montagne = [ 8 , 5 , 4 , 3 , 2 , 1 , '!' ]
 #Montagne = ['!', 2, 8, 2, '!']
 #Montagne = [ 4 , 5 , '!' ]
@@ -26,13 +23,6 @@ def colonne_instable(montagne):
 		S'il n'y a pas de colonnes instables, renvoi False. """
 
 	length = len(montagne)
-
-	# 1) on remplace les '!' par des 0
-	#for i in range(length):
-		#if montagne[i] == '!' :
-			#montagne[i] = 0
-
-	#precedente = montagne[0]
 
 	# si la difference de hauteur est de deux, la colonne est instable
 	for i in range(1,length):
@@ -82,19 +72,15 @@ def choix(montagne,rang):
 		# si il y a une denivelation + importante d'un cote comme de l'autre
 		if gauche != droite :
 			if gauche < droite :
-				#Montagne[rang-1] += 2
 				choix = rang-1
 			else :
-				#Montagne[rang+1] += 2
 				choix = rang+1
 		# ou si la denivelation est la meme de chaque cote
 		else :
 			nbr = choice([-1,1])
-			#Montagne[rang+nbr] += 2
 			choix = rang+nbr
 	# sinon, c'est forcement a droite que l'on procede a l'effondrement
 	else :
-		#print("mur")
 		choix = rang+1
 
 	# on retourne le rang de la colonne qui doit recevoir les 2 unites
@@ -114,7 +100,7 @@ def affichage(montagne):
 		passee en parametre. """
 
 	affichage =  '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
-	#affichage += '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+	affichage += '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
 	#affichage += '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
 
 	# 1) on remplace les '!' par des -1
@@ -144,11 +130,7 @@ def affichage(montagne):
 
 ##### BOUCLE PRINCIPALE #####
 
-i = 0
-
 print(affichage(Montagne))
-
-print(Montagne)
 
 # tant que la structure globale est instable
 while colonne_instable(Montagne) is not False :
@@ -164,130 +146,6 @@ while colonne_instable(Montagne) is not False :
 	else :
 		Montagne[rang] += 2
 
-	i += 1
-
 	print(affichage(Montagne))
 
 print("compteur",compteur)
-
-
-#### TEST DE COLONNE_INSTABLE() ####
-#print([ 8 , 5 , 4 , 3 , 2 , 1 , '!' ])
-#print(colonne_instable([ 8 , 5 , 4 , 3 , 2 , 1 , '!' ]))
-#print([ 6 , 7 , 4 , 3 , 2 , 1 , '!' ])
-#print(colonne_instable([ 6 , 7 , 4 , 3 , 2 , 1 , '!' ]))
-#print(['!', 2, 8, 2, '!'])
-#print(colonne_instable(['!', 2, 8, 2, '!']))
-#print([ 4 , 5 , '!' ])
-#print(colonne_instable([ 4 , 5 , '!' ]))
-#print([ '!', 2, 8, 2, '!'])
-#print(colonne_instable([ '!', 2, 8, 2, '!']))
-
-#### TEST DE AFFICHAGE() ####
-
-#print(affichage([ 8 , 5 , 4 , 3 , 2 , 1 , '!' ]))
-#print(affichage([5,2,'!']))
-#print(affichage(['!', 2, 8, 2, '!']))
-
-#for i in reversed(range(8)):
-	#print(i)
-
-
-#### TEST DE CHOIX() ####
-
-# Montagne = [5,2,'!']
-# print(Montagne)
-# print(choix(0,Montagne))
-
-# Montagne = [3,4,'!']
-# print(Montagne)
-# print(choix(1,Montagne))
-
-# Montagne = [2, 8, 3, '!']
-# print(Montagne)
-# print(choix(1,Montagne))
-
-# Montagne = ['!', 2, 8, 2, '!']
-# print(Montagne)
-# print(choix(2,Montagne))
-
-# Montagne = ['!', 8, '!']
-# print(Montagne)
-# print(choix(1,Montagne))
-
-#print("colonne instable (1) : ",colonne_instable())
-# effondrement(0)
-# print(Montagne)
-# effondrement(1)
-# print(Montagne)
-# effondrement(2)
-# print(Montagne)
-# effondrement(3)
-# print(Montagne)
-# effondrement(4)
-# print(Montagne)
-# effondrement(5)
-# print(Montagne)
-# print(compteur)
-
-#dada = 0
-#def Test():
-#	global dada
-#	dada += 1
-
-#Test()
-
-#print(dada)
-
-#print("colonne instable (2) : ",colonne_instable())
-
-
-# boucle principale
-# tant qu'il y a une colonne instable, on continue
-
-#while colonne_instable() != False :
-
-	#prnt("test")
-
-	# 1/ chercher la premiere colonne instable de gauche a droite
-	#colonne = colonne_instable()
-
-	# 2/ la faire s'effondrer
-	#effondrement(colonne)
-
-#print("compteur : ",compteur)
-
-
-
-# pour le moment, je ne sais pas trop quelle condition mettre,
-# alors on va partir sur une centaine d'iterations pour avoir
-# un systeme stable, ce qui sera largement suffisant
-
-#for i in range(100):
-
-	# je pense qu'il faut proceder comme ceci : 
-	# 2/ la faire s'effondrer
-	# 3/ puis recommencer
-	# 4/ avec a chaque effondrement une verification que le carre n'est
-	#    pas ejecte et donc comptabilise comme ejecte
-
-	# donc visiblement il me faudrait une fonction qui cherche la premiere
-	# colonne instable en partant de gauche
-
-	# puis une fonction pour l'effondrement, qui utilisera la fonction
-	# modifier element
-
-	# et une fonction de verification pour les carres ejectes
-
-	# enfin, il faut un compteur global pour les carres comptes
-	# je le met a l'exterieur de la boucle, juste apres la matrice,
-	# pour que chaque fonction y ait acces
-
-
-
-
-
-
-
-
-
